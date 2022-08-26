4b794d6ab1d141fe85933678dd0f7590b32194d8
Victor Rîbin <rvinowise@gmail.com>
2022-08-26 11:26:35 +0300

    removed parameters

--- fsharp/modules/src/finding_sequences/finding_sequences_cpp.fs
+++ fsharp/modules/src/finding_sequences/finding_sequences_cpp.fs
@@ -11,7 +11,7 @@ module Finding_sequences =
     [<DllImport(
         "C:/prj/ai/modules/finding_sequences/build/Debug/finding_sequences",
          CallingConvention = CallingConvention.Cdecl)>]
-    extern void find_repeated_pairs(string db_connection, int a, float b)
+    extern void find_repeated_pairs(string db_connection)
 
     //let find_repeated_pairs str =
     //    printfn str
