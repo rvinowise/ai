c00da8b4be903a996d16c3fff31fe28c2fa00a1c
victor rîbin <rvinowise@gmail.com>
2022-09-08 19:28:19 +0300

    basic_data_structures.fs

--- fsharp/basic_data_structures/src/Figure/Edge/Edge.fs
+++ fsharp/basic_data_structures/src/Figure/Edge/Edge.fs
@@ -1,5 +1,10 @@
 namespace rvinowise.ai.figure
 
+(* 
+fsharp implementation heavily relies on the database, so, 
+instead of the references to other objects in memory,
+it uses their identifiers in the database (e.g. string id)
+*)
 
 type Edge(head: string, tail: string) = 
     member _.head = head
