dcf94aebd52cd83e484bffa04fd848c7936b4d9b
Victor Rîbin <rvinowise@gmail.com>
2022-09-09 22:35:42 +0300

    sequence finder module

--- modules/finding_sequences/test/catch2/finding_sequences.cpp
+++ modules/finding_sequences/test/catch2/finding_sequences.cpp
@@ -1,5 +1,4 @@
-#ifdef CATCH2_TESTS
-#define CATCH_CONFIG_MAIN  // This tells Catch to provide a main() - only do this in one cpp file
+#define CATCH_CONFIG_MAIN
 #include <catch2/catch_all.hpp>
 
 #include "../Finding"
@@ -36,5 +35,3 @@ TEST_CASE("the same sequences") {
 
     ASSERT_EQ
 }
-
-#endif
