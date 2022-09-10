db070d34693c0644595030f2bf433713fad33e8c
victor rîbin <rvinowise@gmail.com>
2022-09-10 18:43:30 +0300

    c++ not building

--- modules/finding_sequences/test/catch2/finding_sequences.cpp
+++ modules/finding_sequences/test/catch2/finding_sequences.cpp
@@ -1,15 +1,16 @@
-#define CATCH_CONFIG_MAIN
+//#define CATCH_CONFIG_MAIN
 
 #include "Finding_sequences/Finding_sequences.h"
 
-#include <catch2/catch_all.hpp>
+//#include <catch2/catch_all.hpp>
 #include <vector>
+#include <iostream>
 
 using namespace std;
 using namespace rvinowise::ai;
 
-TEST_CASE("the same sequences") {
-    
+//TEST_CASE("the same sequences") {
+int main(int argn, char** args) {
     vector<Interval> appearances_of_a{
         Interval(0,1),
         Interval(2,3),
@@ -29,9 +30,12 @@ TEST_CASE("the same sequences") {
         Interval(4,7)
     };
 
-    // Finding_sequences finding_sequences;
-    // vector<Interval> found_pairs = 
-    //     finding_sequences.find_repeated_pairs(appearances_of_a, appearances_of_b);
+    cout << "test";
+
+    Finding_sequences* finding_sequences = new Finding_sequences();
+    //vector<Interval> found_pairs = 
+    //    finding_sequences.find_repeated_pairs(appearances_of_a, appearances_of_b);
 
     //REQUIRE(found_pairs == appearances_of_ab);
+    return 0;
 }
