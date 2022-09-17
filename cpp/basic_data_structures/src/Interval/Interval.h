3cf3ab59b09fb7b734746bd58b7113bfd8f53865
Victor Rîbin <rvinowise@gmail.com>
2022-09-17 07:43:07 +0300

    interval compare

--- cpp/basic_data_structures/src/Interval/Interval.h
+++ cpp/basic_data_structures/src/Interval/Interval.h
@@ -1,14 +1,20 @@
 #pragma once
 
 #include <stdint.h>
+#include <compare>
+
 
 namespace rvinowise::ai {
 
 struct Interval {
+    public:
     Interval();
     Interval(uint64_t head, uint64_t tail);
+
     uint64_t head;
     uint64_t tail;
+
+    auto operator<=>(const Interval&) const = default;
 }; 
 
 }
