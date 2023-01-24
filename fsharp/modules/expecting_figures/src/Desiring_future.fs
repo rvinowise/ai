329d84fedb8e2e679ceebdd7cb66133545a439ce
victor rîbin <rvinowise@gmail.com>
2023-01-24 14:26:44 +0300

    modified:   fsharp/modules/expecting_figures/src/Desiring_future.fs

--- fsharp/modules/expecting_figures/src/Desiring_future.fs
+++ fsharp/modules/expecting_figures/src/Desiring_future.fs
@@ -21,19 +21,80 @@ namespace rvinowise.ai
     module Desiring_future=
         open rvinowise.ai
 
+        let mood_change_from_start_to_finish
+            (changes: (Moment*Mood) array)
+            (start_index:int)
+            (finish_index:int)
+            =
+            let rec collect_moods_in_interval
+                (changes: (Moment*Mood) array)
+                (start_index:int)
+                (finish_index:int)
+                (accumulated_mood: Mood)
+                =
+                if start_index<finish_index then
+                    collect_moods_in_interval
+                        changes
+                        (start_index+1)
+                        finish_index (
+                            accumulated_mood+(
+                                changes[start_index]|>snd
+                            )
+                        )
+                else
+                    accumulated_mood
+            collect_moods_in_interval
+                (changes: (Moment*Mood) array)
+                (start_index:int)
+                (finish_index:int)
+                (Mood 0)
+
+        let mood_changes_starting_from_index
+            (changes: (Moment*Mood) array)
+            (start_index:int)
+            (found_intervals: ResizeArray<(Interval*Mood)> )
+            =
+            if start_index<changes.Length then
+                found_intervals.Add(
+                    (Interval.regular start_index finish_index),
+                    (
+                        mood_change_from_start_to_finish
+                            changes
+                            start_index
+                            finish_index
+                    )
+                )
+                mood_changes_starting_from_index
+                    changes
+                    (start_index+1)
+                    found_intervals
+            else
+                found_intervals
+
 
         let intervals_changing_mood (mood_changes_history:Mood_changes_history)=
             let changes =
                 mood_changes_history
                 |>extensions.Map.toPairs
                 |>Array.ofSeq
-            changes
-            |>Seq.collecti (fun (previous_moment, mood)->
-                let start_moment = previous_moment+1
+            
+            let rec loop_intervals_changing_mood
+                (changes: (Moment*Mood) array)
+                (start_index:int)
+                (finish_index:int)
+                =
+                if start_index<changes.Length then
+
+            loop_intervals_changing_mood
                 changes
-                |>Seq.
-            )
-            let next_mood_change_interval
+                0
+                0
+            
+            
+            
+
+
+
 
         [<Fact>]
         ``intervals_changing_mood``()=
