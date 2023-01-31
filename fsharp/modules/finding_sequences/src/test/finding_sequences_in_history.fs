0369c00e3a2012ace4fb2f3fc1f1b8bc93206c5e
victor rîbin <rvinowise@gmail.com>
2023-01-31 22:41:38 +0300

    modified:   docs/general_design/operations.png 	deleted:    docs/high_level_optimization/growth_of_algorithms.png 	modified:   fsharp/modules/expecting_figures/commonalities_in_interval.png 	modified:   fsharp/modules/finding_sequences/src/test/finding_sequences_in_history.fs

--- fsharp/modules/finding_sequences/src/test/finding_sequences_in_history.fs
+++ fsharp/modules/finding_sequences/src/test/finding_sequences_in_history.fs
@@ -13,7 +13,7 @@ module finding_sequences_in_history =
     open rvinowise.ai
     open rvinowise.ui
 
-    [<Fact(Skip="ui")>] //
+    [<Fact>] //(Skip="ui")
     let ``visualising stages of pattern finding``()=
         let signal_history =
             built.Event_batches.from_contingent_signals 0 [
@@ -103,6 +103,62 @@ module finding_sequences_in_history =
         |>ui.painted.image.open_image_of_graph
 
     [<Fact>] //(Skip="ui")
+    let ``visualising stages of pattern finding, mixing levels of abstraction``()=
+        let signal_history =
+            built.Event_batches.from_contingent_signals 0 [
+                ["a";"b"];
+                ["c"];
+                ["d";"b"];
+                ["e"];
+                ["f"];
+                ["a"];
+                ["d";"e"];
+                ["e";"f";"g"];
+                ["a";"c"];
+            ]
+        let step1_figure_histories = 
+            signal_history
+            |>built.Event_batches.to_separate_histories
+            |>Separate_histories.figure_histories
+
+        let step2_figure_histories =
+            step1_figure_histories
+            |>Finding_many_repetitions.many_repetitions
+            //|>Seq.append step1_figure_histories
+        let step2_combined_history =
+            signal_history
+            |>built.Event_batches.add_figure_histories step2_figure_histories
+
+        let step3_figure_histories =
+            step2_figure_histories
+            |>Finding_many_repetitions.many_repetitions
+            //|>Seq.append step2_figure_histories
+        let step3_combined_history =
+            step2_combined_history
+            |>built.Event_batches.add_figure_histories step3_figure_histories
+
+        let step4_figure_histories =
+            step3_figure_histories
+            |>Finding_many_repetitions.many_repetitions
+            //|>Seq.append step3_figure_histories
+        let step4_combined_history =
+            step3_combined_history
+            |>built.Event_batches.add_figure_histories step4_figure_histories
+
+        "stages of pattern search mixing all layers of abstraction"
+        |>infrastructure.Graph.empty
+        |>infrastructure.Graph.with_filled_vertex "step 1"
+            (ui.painted.History.add_combined_history signal_history)
+        |>infrastructure.Graph.with_filled_vertex "step 2"
+            (ui.painted.History.add_combined_history step2_combined_history)
+        |>infrastructure.Graph.with_filled_vertex "step 3"
+            (ui.painted.History.add_combined_history step3_combined_history)
+        |>infrastructure.Graph.with_filled_vertex "step 4"
+            (ui.painted.History.add_combined_history step4_combined_history)
+        |>ui.painted.image.open_image_of_graph
+
+
+    [<Fact(Skip="ui")>] //
     let ``visualising stages of pattern finding, with mood``()=
         let signal_history =
             built.Event_batches.from_contingent_signals 0 [
