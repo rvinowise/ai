db38ab211396149dd1cf423e3688ea3b73879d7b
Victor Rîbin <rvinowise@gmail.com>
2023-02-21 06:35:27 +0300

    modified:   fsharp/modules/finding_figures/commonalities_in_sequence_groups.png 	modified:   fsharp/modules/finding_figures/src/commonalities_in_sequence_groups.fs

--- fsharp/modules/finding_figures/src/commonalities_in_sequence_groups.fs
+++ fsharp/modules/finding_figures/src/commonalities_in_sequence_groups.fs
@@ -4,14 +4,16 @@ module rvinowise.ai.Finding_figures
         it's similar to stencil application, but we map a source-figure onto the target-figure
      *)
 
-    let convert_appearances_in_interval_to_figure
-        (appearances: Sequence_appearances seq)
+    
+
+    let event_batches_to_figure
+        (batches: Event_batches)
         =()
 
 
-    let commonalities_between_groups_of_sequences
-        (appearances1: Sequence_appearances seq)
-        (appearances2: Sequence_appearances seq)
+    let commonalities_between_figures
+        (figure1: Figure)
+        (figure2: Figure)
         =()
 
     
