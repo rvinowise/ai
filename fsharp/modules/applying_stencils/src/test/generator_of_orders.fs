5a93d5ab8845a7be586e2da73e12c74d08f45ed0
Victor Rîbin <rvinowise@gmail.com>
2023-03-26 00:28:25 +0300

    modified:   fsharp/modules/applying_stencils/src/test/generator_of_orders.fs

--- fsharp/modules/applying_stencils/src/test/generator_of_orders.fs
+++ fsharp/modules/applying_stencils/src/test/generator_of_orders.fs
@@ -102,8 +102,74 @@ module ``generator of orders``=
                     Element_to_target<Vertex_id, Vertex_id>((Vertex_id "A2"),(Vertex_id "a2"));
                 ];
                 [
+                    Element_to_target<Vertex_id, Vertex_id>((Vertex_id "B1"),(Vertex_id "b1"));
+                    Element_to_target<Vertex_id, Vertex_id>((Vertex_id "B2"),(Vertex_id "b2"));
+                ]
+            };
+        ]
+        
+    [<Fact>]
+    let ``using generator_of_orders in f# average``()=    
+        let generator = Generator_of_orders<seq<Element_to_target<Vertex_id, Vertex_id>>> [
+            Generator_of_individualised_mappings<Vertex_id,Vertex_id> (seq{
+                struct (
+                    (Vertex_id "A1"),
+                    seq{(Vertex_id "a1");(Vertex_id "a2")}
+                );
+                
+                struct (
+                    (Vertex_id "A2"),
+                    seq{(Vertex_id "a1");(Vertex_id "a2")}
+                );
+
+            });
+            Generator_of_individualised_mappings<Vertex_id,Vertex_id> (seq{
+                struct ((Vertex_id "B1"), seq{(Vertex_id "b1");(Vertex_id "b2")});
+                struct ((Vertex_id "B2"), seq{(Vertex_id "b1");(Vertex_id "b2")});
+            });
+        ]
+    
+        generator
+        |>Seq.take 2
+        |>should equal [
+            seq {
+                [
+                    Element_to_target<Vertex_id, Vertex_id>((Vertex_id "A1"),(Vertex_id "a2"));
+                    Element_to_target<Vertex_id, Vertex_id>((Vertex_id "A2"),(Vertex_id "a1"));
+                ]; 
+                [
                     Element_to_target<Vertex_id, Vertex_id>((Vertex_id "B1"),(Vertex_id "b2"));
                     Element_to_target<Vertex_id, Vertex_id>((Vertex_id "B2"),(Vertex_id "b1"));
+                ]
+            };
+            seq {
+                [
+                    Element_to_target<Vertex_id, Vertex_id>((Vertex_id "A1"),(Vertex_id "a1"));
+                    Element_to_target<Vertex_id, Vertex_id>((Vertex_id "A2"),(Vertex_id "a2"));
+                ]; 
+                [
+                    Element_to_target<Vertex_id, Vertex_id>((Vertex_id "B1"),(Vertex_id "b2"));
+                    Element_to_target<Vertex_id, Vertex_id>((Vertex_id "B2"),(Vertex_id "b1"));
+                ]
+            };
+            seq {
+                [
+                    Element_to_target<Vertex_id, Vertex_id>((Vertex_id "A1"),(Vertex_id "a2"));
+                    Element_to_target<Vertex_id, Vertex_id>((Vertex_id "A2"),(Vertex_id "a1"));
+                ];
+                [
+                    Element_to_target<Vertex_id, Vertex_id>((Vertex_id "B1"),(Vertex_id "b1"));
+                    Element_to_target<Vertex_id, Vertex_id>((Vertex_id "B2"),(Vertex_id "b2"));
+                ]
+            }
+            seq {
+                [
+                    Element_to_target<Vertex_id, Vertex_id>((Vertex_id "A1"),(Vertex_id "a1"));
+                    Element_to_target<Vertex_id, Vertex_id>((Vertex_id "A2"),(Vertex_id "a2"));
+                ];
+                [
+                    Element_to_target<Vertex_id, Vertex_id>((Vertex_id "B1"),(Vertex_id "b1"));
+                    Element_to_target<Vertex_id, Vertex_id>((Vertex_id "B2"),(Vertex_id "b2"));
                 ]
             };
         ]
