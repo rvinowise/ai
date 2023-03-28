0b6407a70ea8ecc0ac7e68491b8bc339729da379
victor rîbin <rvinowise@gmail.com>
2023-03-28 19:20:00 +0300

    new file:   fsharp/modules/applying_stencils/src/test/benchmarks/mapping_first_nodes.fs 	modified:   fsharp/basic_data_structures/src/Graphs/Figure/Figure_example.fs 	modified:   fsharp/basic_data_structures/src/Graphs/Figure/Figure_tests.fs 	modified:   fsharp/basic_data_structures/src/Graphs/Stencil/Stencil_example.fs 	modified:   fsharp/modules/applying_stencils/applying_stencils.fsproj 	deleted:    fsharp/modules/applying_stencils/src/Applying_stencil.fs 	modified:   fsharp/modules/applying_stencils/src/test/applying_a_stencil.fs 	modified:   fsharp/modules/applying_stencils/src/test/benchmarks/mapping_first_nodes.fs 	modified:   fsharp/modules/applying_stencils/src/test/benchmarks/querying_stencil.fs 	modified:   fsharp/modules/applying_stencils/src/test/generator_of_orders.fs 	modified:   modules/applying_stencils/enumerating_combinations/src/Generator_of_mappings.cs 	modified:   modules/applying_stencils/enumerating_combinations/src/Generator_of_orders.cs 	modified:   modules/applying_stencils/enumerating_combinations/test/Generator_of_mappings/Generator_of_mappings.cs 	modified:   modules/applying_stencils/enumerating_combinations/test/Generator_of_orders/Generator_of_orders.cs 	fsharp/modules/applying_stencils/src/Applying_stencil/ 	fsharp/modules/applying_stencils/src/test/benchmarks/mapping_first_nodes.png

---
+++ fsharp/modules/applying_stencils/src/test/benchmarks/mapping_first_nodes.fs
@@ -0,0 +1,34 @@
+namespace rvinowise.ai.test
+
+open BenchmarkDotNet.Engines
+open Xunit
+
+open BenchmarkDotNet.Configs
+open BenchmarkDotNet.Attributes
+open BenchmarkDotNet.Running
+
+open rvinowise.ai
+
+
+
+module enumerating_combinations =
+
+    type Benchmarking_enumerating_combinations() =
+
+        [<Benchmark>]
+        member this.generator_of_mappings_with_indices()=
+            Applying_stencil.``map_first_nodes(breaking recursion)`` 
+                example.Stencil.a_fitting_stencil
+                example.Figure.a_high_level_relatively_simple_figure
+            |> Consumer().Consume
+  
+
+    [<Fact(Skip="slow")>] //
+    let run_benchmark()=
+
+        let config = 
+            DefaultConfig.Instance.
+                WithOptions(ConfigOptions.DisableOptimizationsValidator)
+
+        BenchmarkRunner.Run<Benchmarking_enumerating_combinations>(config) |> ignore
+
