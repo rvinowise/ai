a3771fffff63308f81c427c7f9fcc0939329a754
victor rîbin <rvinowise@gmail.com>
2023-04-15 12:34:17 +0300

    modified:   fsharp/basic_data_structures/basic_data_structures.fsproj 	modified:   fsharp/basic_data_structures/src/Graphs/Figure/built/Renaming_figures.fs

--- fsharp/basic_data_structures/src/Graphs/Figure/built/Renaming_figures.fs
+++ fsharp/basic_data_structures/src/Graphs/Figure/built/Renaming_figures.fs
@@ -12,13 +12,14 @@ module rvinowise.ai.Renaming_figures
     
     let vertices_with_sequencial_names
         (referenced_figure: Figure_id)
+        (starting_number)
         (amount:int)
         =
         List.init amount (fun number->
             (
                 (Figure_id.value referenced_figure)
                 +
-                (string (number+1))
+                (string (number+starting_number))
             )
             |>Vertex_id
         )
@@ -103,45 +104,79 @@ module rvinowise.ai.Renaming_figures
     let assign_next_numbers_to_sorted_vertices
         (all_renamings: Map<Vertex_id, Vertex_id>)
         (all_figures_to_last_number: Map<Figure_id, int>)
-        (figure: Figure_id)
+        (referenced_figure: Figure_id)
         (vertices: Vertex_id list)
         =
         let last_number = 
             all_figures_to_last_number
-            |>Map.tryFind figure
+            |>Map.tryFind referenced_figure
             |>Option.defaultValue 0
 
-        let renamings=
-            figures
-            |>Seq.map (fun pair->
-                let figure = pair.Key
-                let vertex = pair.Value
-                let new_last_number = all_figures_to_last_number.TryFind(figure)|>(Option.defaultValue 0)
-                
-                (vertex,
-                figure|>next_vertex_id_for_figure new_last_number,
-                figure,
-                new_last_number)
+        let new_renamings =
+            vertices
+            |>List.length
+            |>vertices_with_sequencial_names 
+                referenced_figure
+                last_number
+            |>Seq.zip vertices
+
+        new_renamings
+        |>Seq.fold (fun map added->
+            map|>Map.add (fst added) (snd added)
             )
-        let updated_renamings =
-            Seq.fold 
-                (fun map (old_name,new_name,_,_) ->
-                    Map.add old_name new_name map
-                )
-                all_renamings
-                renamings
-        let updated_figures_to_last_number = 
-            all_figures_to_last_number
-            |>Map.add 
-                figure 
-                (last_number+(vertices|>List.length))
-        (updated_renamings,updated_figures_to_last_number)
+            all_renamings
+        ,
+        all_figures_to_last_number
+        |>Map.add referenced_figure (last_number+(List.length vertices))
+
+
+    let form_for_comparison 
+        (subfigures:Map<Vertex_id, Figure_id>)
+        (renamings: Map<Vertex_id, Vertex_id>)
+        (vertices: Vertex_id list)
+        =
+        vertices
+        |>List.map(fun vertex->
+            renamings
+            |>Map.tryFind vertex
+            |>function
+            |Some renamed_vertex->renamed_vertex|>Vertex_id.value
+            |None->subfigures[vertex]|>Figure_id.value
+        )|>List.sort
+
+    let compare_compeating_vertices
+        (owner_figure:Figure)
+        (renamings: Map<Vertex_id, Vertex_id>)
+        vertex1 vertex2
+        = 
+        let rec compare_neighbors
+            (neighbors1: Vertex_id list)
+            (neighbors2: Vertex_id list)
+            =
+            match neighbors1,neighbors2 with
+            |[],[]->0
+            |_->
+                let comparison =
+                    neighbors1
+                    |>form_for_comparison owner_figure.subfigures renamings
+                    |>Seq.compareWith Operators.compare (
+                        neighbors2
+                        |>form_for_comparison owner_figure.subfigures renamings
+                    )
+                match comparison with 
+                |0->
+
+                |difference->difference
+
+
 
     let sort_compeating_vertices_by_their_neighbours
         (owner_figure:Figure)
         (renamings: Map<Vertex_id, Vertex_id>)
         (vertices: Vertex_id list)
-        =()
+        =
+        vertices
+        |>List.sortWith (compare_compeating_vertices owner_figure renamings)
 
     let rec assign_numbers_to_next_vertex_wave
         (owner_figure:Figure)
@@ -187,24 +222,20 @@ module rvinowise.ai.Renaming_figures
                     figure
                     vertices
             )
-            (renamings,figures_to_last_number)
+                (renamings,figures_to_last_number)
 
-        |>Seq.map(fun pair->
-            let figure = pair.Key
-            let vertices = pair.Value
-        )
 
         let next_vertices = 
             vertices
             |>Seq.collect (Edges.next_vertices owner_figure.edges)
             |>List.ofSeq
         match next_vertices with
-        |[]->(renamings, figure_to_last_number)
+        |[]->(renamings, figures_to_last_number)
         |next_vertices->
             assign_numbers_to_next_vertex_wave
                 owner_figure
                 renamings
-                figure_to_last_number
+                figures_to_last_number
                 next_vertices
         
     let rename_vertices_to_standard_names 
@@ -229,12 +260,12 @@ module rvinowise.ai.Renaming_figures
         {
             edges=
                 owner_figure.edges
-                |>renamed_edges_for_figure vertices_to_new_names
+                |>renamed_edges_for_figure renamings
                 |>Set.ofSeq
             
             subfigures=
                 owner_figure.subfigures
-                |>renamed_subfigures_for_figure vertices_to_new_names
+                |>renamed_subfigures_for_figure renamings
         }
 
     [<Fact>]
