99c62148e9fb5f2a85fa5222cd9b93c45e8e594f
Victor Rîbin <rvinowise@gmail.com>
2023-05-09 18:57:17 +0300

    new file:   examples/math/.idea/.idea.math.dir/.idea/.gitignore 	new file:   examples/math/.idea/.idea.math.dir/.idea/encodings.xml 	new file:   examples/math/.idea/.idea.math.dir/.idea/indexLayout.xml 	new file:   examples/math/.idea/.idea.math.dir/.idea/misc.xml 	new file:   examples/math/.idea/.idea.math.dir/.idea/vcs.xml 	modified:   examples/math/src/test/math_primers.fs

--- examples/math/src/test/math_primers.fs
+++ examples/math/src/test/math_primers.fs
@@ -12,25 +12,24 @@ module Math_primers=
 
     [<Fact>]
     let ``find sequences in math primers``()=
-        let input_primers =
-            built.Event_batches.from_text_blocks [
-                "N:0,1,2,3,4,5,6,7,8,9;";
-                "1+1=2;×"; "1+2=3;×";
-                "1+1=";
-    //mom:       0123456789¹123456789²1
-    //mom(20+):  2345678    9³12345
-    //mom(30+):  6789
-            ]
-            |>built.Event_batches.from_text
-            |>built.Event_batches.to_sequence_appearances
-            |>Finding_many_repetitions.all_repetitions
-            |>Set.ofSeq
-            |>Set.isProperSubset (
-                [
-                    built.Sequence_appearances.from_string_and_pairs "1+1=" [0,23;8,27];
-                    built.Sequence_appearances.from_string_and_pairs "1+1=" [1,21;7,26];
-                ]|>Set.ofSeq
-            ) |>should equal true
+        [
+            "N:0,1,2,3,4,5,6,7,8,9;";
+            "1+1=2;×"; "1+2=3;×";
+            "1+1=";
+//mom:       0123456789¹123456789²1
+//mom(20+):  2345678    9³12345
+//mom(30+):  6789
+        ]
+        |>built.Event_batches.from_text_blocks
+        |>built.Event_batches.to_sequence_appearances
+        |>Finding_many_repetitions.all_repetitions
+        |>Set.ofSeq
+        |>Set.isProperSubset (
+            [
+                built.Sequence_appearances.from_string_and_pairs "1+1=" [22,25;36,39];
+                built.Sequence_appearances.from_string_and_pairs "1+=;×" [22,28;29,35];
+            ]|>Set.ofSeq
+        ) |>should equal true
 
     [<Fact>]
     let ``draw training history``()=
