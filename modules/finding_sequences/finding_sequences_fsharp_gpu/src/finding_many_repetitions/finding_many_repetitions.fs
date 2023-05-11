67a525ec716f59c1ca776cc479151a7808d7541c
Victor Rîbin <rvinowise@gmail.com>
2023-05-12 01:14:26 +0300

    box

--- modules/finding_sequences/finding_sequences_fsharp_gpu/src/finding_many_repetitions/finding_many_repetitions.fs
+++ modules/finding_sequences/finding_sequences_fsharp_gpu/src/finding_many_repetitions/finding_many_repetitions.fs
@@ -12,14 +12,6 @@ module ``Finding_many_repetitions(fsharp_gpu)`` =
     open ILGPU.Runtime
 
 
-    type Program =
-        static member MyKernel 
-            (index: Index1D) 
-            (buffer: ArrayView1D<int, Stride1D.Dense>) 
-            (constant: int) 
-            =
-            buffer.[index] <- int(index) + constant
-
     type Known_sequences = Set<Figure_id array>
 
     let private is_this_sequence_already_found 
@@ -29,50 +21,30 @@ module ``Finding_many_repetitions(fsharp_gpu)`` =
         known_sequences
         |>Set.contains ab_sequence 
 
-    let many_repetitions
-        (sequence_appearances: seq<Sequence_appearances>)
-        =
-        let known_sequences = 
-            sequence_appearances
-            |>Seq.map (fun history->history.sequence)
-            |>Set.ofSeq
-        
-        (sequence_appearances,sequence_appearances)
-        ||>Seq.allPairs
-        |>Seq.fold (
-            fun 
-                (known_sequences, found_pairs)
-                (a_history,b_history) 
-                ->
-            let ab_sequence = 
-                Array.append 
-                    a_history.sequence
-                    b_history.sequence
-            if is_this_sequence_already_found
-                known_sequences
-                ab_sequence
-            then
-                known_sequences,found_pairs
-            else
-                let found_pair = 
-                    (a_history.appearances, b_history.appearances)
-                    |>``Finding_repetitions(fsharp_gpu)``.repeated_pair_with_histories ab_sequence
-                if Appearances.has_repetitions found_pair.appearances then
-                    (
-                        known_sequences
-                        |>Set.add found_pair.sequence
-                    ),
-                    found_pair::found_pairs
-                else
-                    known_sequences,found_pairs
-            )
-            (known_sequences,[])
-        |>snd
-    
+    type Program =
+        static member kernel 
+            (index: Index1D) 
+            (appearances: ArrayView1D<Interval, Stride1D.Dense>) 
+            (buffer: ArrayView1D<int, Stride1D.Dense>) 
+            =
+            buffer.[index] <- int(index) + constant
 
     let all_repetitions 
         (sequence_appearances: seq<Sequence_appearances>)
         =
+        let context = Context.CreateDefault();
+        let accelerator = 
+            context.GetPreferredDevice(preferCPU: false).CreateAccelerator(context);
+        
+        use appearances_buffer = 
+            accelerator.Allocate1D [0; 1; 2; 3; 4; 5; 6; 7; 8; 9]
+        use deviceOutput = accelerator.Allocate1D<int>(10_000);
+
+        let loadedKernel = 
+            accelerator.LoadAutoGroupedStreamKernel<_,_,_>(kernel)
+
+        loadedKernel(Index1D(int(deviceOutput.Length)), deviceData.View, deviceOutput.View);
+
         let rec steps_of_finding_repetitions
             (all_sequences: seq<Sequence_appearances>)
             (sequences_of_previous_step: seq<Sequence_appearances>)
