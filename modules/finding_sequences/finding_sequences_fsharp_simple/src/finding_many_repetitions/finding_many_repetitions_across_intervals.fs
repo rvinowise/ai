e5acc37c4bfae8089066805cb41e8ab589a20ad0
victor rîbin <rvinowise@gmail.com>
2023-06-07 14:06:33 +0300

    modified:   modules/expecting_figures/expecting_figures.fsproj 	modified:   modules/expecting_figures/src/Desiring_future.fs 	modified:   modules/finding_sequences/finding_sequences_fsharp_simple/src/finding_repetitions/finding_repetitions_across_intervals.fs 	modified:   modules/finding_sequences/finding_sequences_interface/src/finding_many_repetitions.fs

--- modules/finding_sequences/finding_sequences_fsharp_simple/src/finding_many_repetitions/finding_many_repetitions_across_intervals.fs
+++ modules/finding_sequences/finding_sequences_fsharp_simple/src/finding_many_repetitions/finding_many_repetitions_across_intervals.fs
@@ -5,11 +5,35 @@ open System
 
 module ``Finding_many_repetitions_across_intervals(simple)`` =
 
+    let take_which_exist_in_other_interval 
+        (other_interval: (Sequence*Interval array) seq)
+        (this_interval: (Sequence*Interval array) seq)
+        =
+        this_interval
+        |>Seq.filter (fun (sequence,_)->
+            other_interval|>Seq.exists (fst>>(=)sequence)
+        )
     
+    let take_commonalities_between_2_intervals
+        (interval1_appearances: (Sequence*Interval array) seq) 
+        (interval2_appearances: (Sequence*Interval array) seq)
+        =
+        let interval1_history = 
+            interval1_appearances
+            |>take_which_exist_in_other_interval interval2_appearances
+        let interval2_history = 
+            interval2_appearances
+            |>take_which_exist_in_other_interval interval1_appearances
+        interval1_history,interval2_history
 
-    let repetitions_across_intervals
+    let repetitions_in_2_intervals
         halves_can_form_pair
         (interval1_appearances: (Sequence*Interval array) seq) 
         (interval2_appearances: (Sequence*Interval array) seq)
         =
+        let history1,
+            history2 = 
+                take_commonalities_between_2_intervals
+                    interval1_appearances
+                    interval2_appearances
         ()
